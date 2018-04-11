3e971031764861da573a610b4b5ac950777bbb83
Felicia Tsakonas <felicia.tsakonas@data-experts.de>
2018-04-11 12:08:59 +0200

    PersistenceManager mal spaßenshalber gefüllt

--- src/de/hwrberlin/friendsforfun/persistence/PersistenceManager.java
+++ src/de/hwrberlin/friendsforfun/persistence/PersistenceManager.java
@@ -1,9 +1,21 @@
 package de.hwrberlin.friendsforfun.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PersistenceManager {
 
-	public PersistenceManager() {
-		
+	public static void main(String[] args) throws SQLException {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class.forName : " + e.getMessage());
+		}
+		String url = "jdbc:mariadb://localhost:3306/test2";
+		Connection con = DriverManager.getConnection(url, "root", "root");
+
 	}
-	
+
 }
