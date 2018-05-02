881744c871b63af3a3e207184c2b447f22a6a1d4
Felicia Tsakonas <felicia.tsakonas@data-experts.de>
2018-05-02 09:37:39 +0200

    zumindest läuft jetzt die index seite (naja) bei tomcat

--- src/main/java/de/hwrberlin/FriendsForFun/common/HomepageController.java
+++ src/main/java/de/hwrberlin/FriendsForFun/common/HomepageController.java
@@ -2,16 +2,14 @@ package de.hwrberlin.FriendsForFun.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomepageController {
 
-	 @GetMapping("/homepage.html")
-	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-	        model.addAttribute("name", name);
-	        return "homepage";
-	    }
-	
+    @RequestMapping({"/homepage.html "})
+    public String index(Model model){
+        return "homepage";
+    }
+
 }
