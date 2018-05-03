785f8ddfae30a31eda806ca9acbb93c938792ac2
AG1601 <alicia.guhl@arcor.de>
2018-05-03 12:38:40 +0200

    Daten aus DB werden auf homepage.html ausgegeben

--- src/main/java/de/hwrberlin/FriendsForFun/common/HomepageController.java
+++ src/main/java/de/hwrberlin/FriendsForFun/common/HomepageController.java
@@ -1,11 +1,15 @@
 package de.hwrberlin.FriendsForFun.common;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import de.hwrberlin.FriendsForFun.persistence.entities.Event;
 import de.hwrberlin.FriendsForFun.persistence.manager.EventManager;
 
 @Controller
@@ -20,8 +24,9 @@ public class HomepageController {
     }
 
     @GetMapping("/homepage.html")
-    public Object[] getEvents() {
-    	return eventmanager.getEvents().toArray();
+    @ModelAttribute("events")
+    public List<Event> getEvents() {
+    	return eventmanager.getEvents();
     }
     
 }
