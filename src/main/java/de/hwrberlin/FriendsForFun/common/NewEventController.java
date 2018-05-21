fdf991bc3eac0c7b15e84c3ccb52bb3c97238d0a
Felicia Tsakonas <felicia.tsakonas@data-experts.de>
2018-05-21 14:52:49 +0200

    gaaaanz grob kann man ein event anlegen *hust*

--- src/main/java/de/hwrberlin/FriendsForFun/common/NewEventController.java
+++ src/main/java/de/hwrberlin/FriendsForFun/common/NewEventController.java
@@ -1,17 +1,34 @@
 package de.hwrberlin.FriendsForFun.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import de.hwrberlin.FriendsForFun.persistence.entities.Event;
+import de.hwrberlin.FriendsForFun.persistence.entities.Nutzer;
+import de.hwrberlin.FriendsForFun.persistence.manager.EventManager;
+import de.hwrberlin.FriendsForFun.persistence.manager.NutzerManager;
 
 @Controller
 public class NewEventController {
+	
+	@Autowired
+	private EventManager eventManager;
 
-	 @GetMapping("/neues_event.html")
-	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-	        model.addAttribute("name", name);
+	@PostMapping("/event/add")
+	public String addNutzer(@ModelAttribute ("event") Event event, BindingResult result) {
+		eventManager.createObject(event);
+		return "homepage.html";
+	}
+	
+		@GetMapping("/neues_event.html")
+		public String addEvent(Model model) {
+	        model.addAttribute("event", new Event());
 	        return "neues_event";
 	    }
-	
 }
