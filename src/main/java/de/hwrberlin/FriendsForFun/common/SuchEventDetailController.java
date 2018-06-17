0149f490e19330257779f22a5cb2d75e40e83b5d
Felicia Tsakonas <felicia.tsakonas@data-experts.de>
2018-06-17 13:39:02 +0200

    ich glaube die detailseite funzt jetzt

---
+++ src/main/java/de/hwrberlin/FriendsForFun/common/SuchEventDetailController.java
@@ -0,0 +1,29 @@
+package de.hwrberlin.FriendsForFun.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import de.hwrberlin.FriendsForFun.persistence.entities.Event;
+import de.hwrberlin.FriendsForFun.persistence.manager.EventManager;
+
+@Controller
+@SessionAttributes("event")
+public class SuchEventDetailController {
+	
+	@Autowired
+	EventManager eventManager;
+
+	@RequestMapping("/detaileventSuche.html")
+	public String details(@ModelAttribute("id") int id, Model model) {
+		System.out.println("Event: " + eventManager.getEventById(id));
+		model.addAttribute("event", eventManager.getEventById(id));
+		return "detaileventSuche.html";
+	}
+
+}
