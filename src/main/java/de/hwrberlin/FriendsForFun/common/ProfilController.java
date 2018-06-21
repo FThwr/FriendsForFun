293cf72ff178ce2074827c168dd30e6d01d100d2
Maximilian <Maximilian@192.168.187.202>
2018-06-21 13:36:43 +0200

    Profil entsprechende hinweise, sidebaar anpassungen, kleinigkeiten

--- src/main/java/de/hwrberlin/FriendsForFun/common/ProfilController.java
+++ src/main/java/de/hwrberlin/FriendsForFun/common/ProfilController.java
@@ -1,6 +1,8 @@
 
 package de.hwrberlin.FriendsForFun.common;
 
+import javax.persistence.NoResultException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,35 +20,53 @@ import de.hwrberlin.FriendsForFun.persistence.manager.NutzerManager;
 @SessionAttributes("nutzer")
 public class ProfilController {
 
+	static String message;
+	static int counter;
+
 	@Autowired
 	private NutzerManager nutzerManager;
 
-	@RequestMapping("/profil.html")
-	public String index(Model model) {
-		return "profil.html";
-	}
-
 	@GetMapping("/profil.html")
 	public Nutzer getAktivNutzer(@ModelAttribute("nutzer") Nutzer nutzer, BindingResult result, Model model) {
+		System.out.println("counter: " + counter + message);
+		if (counter == 1) {
+			message = "Änderungen erfolgreich!";
+		}
+		if (counter == 2) {
+			message = "Username konnte nicht geändert werden, da er bereits existiert!";
+		}
+		if (counter == 0) {
+			message = "";
+		}
+		model.addAttribute("myMessage", message);
+		counter = 0;
 		return nutzerManager.getNutzer(nutzer.getUsername(), nutzer.getPasswort()) /* .get(1) */;
+
 	}
 
 	@PostMapping("/profil.html")
 	public String changeUsersettings(@ModelAttribute("nutzer") Nutzer nutzer,
-			@ModelAttribute("passwort") String password, @ModelAttribute("username") String username, @ModelAttribute("mail") String mail,
-			BindingResult result, Model model) {
+			@ModelAttribute("passwort") String password, @ModelAttribute("user") String username,
+			@ModelAttribute("mail") String mail, BindingResult result, Model model) {
+		counter = 1;
 		if (!password.isEmpty()) {
 			nutzer.setPasswort(password);
 		}
 		if (!username.isEmpty()) {
-			nutzer.setUsername(username);
+			try {
+				nutzerManager.getNutzer(username);
+				counter = 2;
+			} catch (NoResultException e) {
+				nutzer.setUsername(username);
+			}
 		}
-		if(!mail.isEmpty()) {
+		if (!mail.isEmpty()) {
 			nutzer.setMail(mail);
 		}
 		if (!username.isEmpty() || !password.isEmpty() || !mail.isEmpty()) {
+
 			nutzerManager.saveObject(nutzer);
 		}
-		return "passwortaenderung.html";
+		return "redirect:/profil.html";
 	}
 }
